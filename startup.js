9def712ffbd9571e388c081233ab27eeb8ec3385
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-14 20:10:39 +0200

    startup file connecting to db and creating db

--- startup.js
+++ startup.js
@@ -1,22 +1,28 @@
+/**
+ * description: create db mysql with name hotel_reservation
+ */
 async function startup() {
   var mysql = require("mysql");
+
+  // create connection to mysql
   var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
-    database: "hotel_Reservation"
+    password: ""
   });
 
-  connection.connect();
+  // try to connect throw error if cannot connect
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 
-  connection.query("SELECT 1", function(error, results, fields) {
-    if (error) {
-      throw error;
-    } else {
-      console.log("connected as id " + connection.threadId);
-    }
-    // connected!
+  //query to create the database which we will add the tables to
+  connection.query("CREATE DATABASE hotel_reservation", function(err, result) {
+    if (err) console.log("Database exists!");
+    else console.log("Database created!");
   });
 }
 
+startup();
 module.exports = startup;
