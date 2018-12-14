1d98be52c98a5077421b65f45e16296ff9542be6
ramikhafagi96 <ramikh.developer@gmail.com>
2018-12-14 21:24:30 +0200

    TABLE CREATION LAST INSHA'ALLAH

--- database.sql
+++ database.sql
@@ -3,8 +3,10 @@ CREATE DATABASE hotel_reservation;
 CREATE TABLE user (
     username varchar(255) NOT NULL PRIMARY KEY,
     email varchar(255) UNIQUE NOT NULL,
-    name varchar(255) NOT NULL,
     user_password varchar(255) NOT NULL,
+    name varchar(255) NOT NULL,
+    phone varchar(255) UNIQUE NOT NULL,
+    ssn varchar(255) UNIQUE NOT NULL,
     birthdate date NOT NULL,
     address varchar(255) NOT NULL,
     class bit DEFAULT 0,
@@ -17,14 +19,12 @@ CREATE TABLE hotel(
     hotel_branch varchar(255) NOT NULL,
     location varchar(255) NOT NULL,
     image LONGBLOLB NOT NULL,
-    stars INT,
-    rating TEXT DEFAULT '',
     broker_user varchar(255),
     owner_user varchar(255),
     suspended bit DEFAULT 0,
-    CONSTRAINT PK_HOTEL PRIMARY KEY (hotel_number,hotel_branch),
     FOREIGN KEY (broker_user) REFERENCES user(username),
-    FOREIGN KEY (owner_user) REFERENCES user(username)
+    FOREIGN KEY (owner_user) REFERENCES user(username),
+    CONSTRAINT PK_HOTEL PRIMARY KEY (hotel_number,hotel_branch)
 );
 
 CREATE TABLE room(
@@ -34,11 +34,36 @@ CREATE TABLE room(
     state bit DEFAULT 0,
     room_type varchar(255) NOT NULL,
     price float NOT NULL,
-    CONSTRAINT PK_ROOM PRIMARY KEY (room_number,hotel_branch,hotel_number),
     FOREIGN KEY (hotel_branch) REFERENCES hotel(hotel_branch),
-    FOREIGN KEY (hotel_number) REFERENCES hotel(hotel_number)
+    FOREIGN KEY (hotel_number) REFERENCES hotel(hotel_number),
+    CONSTRAINT PK_ROOM PRIMARY KEY (room_number,hotel_branch,hotel_number)
 );
 
 CREATE TABLE rating(
-    
+    rating float DEFAULT 0,
+    hotel_number INT NOT NULL,
+    hotel_branch varchar(255) NOT NULL,
+    customer_user varchar(255) NOT NULL,
+    FOREIGN KEY (hotel_branch) REFERENCES hotel(hotel_branch),
+    FOREIGN KEY (hotel_number) REFERENCES hotel(hotel_number)
+    CONSTRAINT PK_RATING PRIMARY KEY (hotel_number,hotel_branch),
+    CHECK (rating BETWEEN 0 AND 5)
+);
+
+CREATE TABLE reservation(
+    res_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    start_date date,
+    end_date date,
+    check_in bit DEFAULT 0,
+    check_out bit DEFAULT 0
+);
+
+CREATE TABLE reservation_relation(
+    customer_user varchar(255) NOT NULL,
+    hotel_number INT NOT NULL,
+    hotel_branch varchar(255) NOT NULL,
+    res_id INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY (customer_user) REFERENCES user(username),
+    FOREIGN KEY (hotel_number) REFERENCES hotel(hotel_number),
+    FOREIGN KEY (hotel_branch) REFERENCES hotel(hotel_branch)
 );
