0eb93350a594784c9a7c2cba7e9e4dbdcadbf5cd
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-14 20:36:58 +0200

    index working and starting server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const connect = require("./connect");
+const app = require("./app");
+
+const hostname = "127.0.0.1";
+const port = process.env.port || 3000;
+
+app.listen(port, hostname, function() {
+  console.log("server is up and running");
+  connect();
+});
