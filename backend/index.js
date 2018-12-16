ca1ebb58210596b0d04e15237b627bdec186f919
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-16 21:07:54 +0200

    Changed appropriately to call databseCreate query

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
+// connection to mysql
 const connect = require("./connect");
+
+// our backend app
 const app = require("./app");
+
+// our database creation query
+const dbQueries = require("./queries");
 
 const hostname = "127.0.0.1";
 const port = process.env.port || 3000;
 
 app.listen(port, hostname, function() {
   console.log("server is up and running");
+
   connect();
+
+  dbQueries.databaseCreate("hotel_reservation");
 });
