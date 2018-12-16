5a6601169b44c90418ea8ca29cdc7fc92cd2b0d9
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-16 21:07:07 +0200

    connect is only responsible for handing over a connection

--- backend/connect.js
+++ backend/connect.js
@@ -1,30 +1,26 @@
+const mysql = require("mysql");
+let connection;
+
 /**
- * description: create db mysql with name hotel_reservation
+ * description: create and return connection to DB
+ * note: need to be sync as other queries depend on the db creation
  */
-async function connect() {
-  var mysql = require("mysql");
+function connect() {
+  // We need to export our connection so making it a variable and exporting it
 
-  // create connection to mysql
-  var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: ""
-  });
-
-  // try to connect throw error if cannot connect
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-
-  //query to create the database which we will add the tables to
-  connection.query(
-    "CREATE DATABASE hotel_reservation CHARACTER SET utf8 COLLATE utf8_general_ci",
-    function(err, result) {
-      if (err) console.log("Database exists!");
-      else console.log("Database created!");
-    }
-  );
+  if (!connection) {
+    connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: ""
+    });
+    // try to connect throw error if cannot connect
+    connection.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+    });
+  }
+  return connection;
 }
 
 module.exports = connect;
