91531d33ce645122f074ba0ce1603c20ec0bbe0d
Elzawawy <amr22.elzawawy@ymail.com>
2018-12-16 03:48:48 +0200

    NOT NULL added to room and hotel. Boolean varaible convention re-named.

--- backend/database.sql
+++ backend/database.sql
@@ -9,24 +9,24 @@ CREATE TABLE user (
     ssn varchar(255) UNIQUE NOT NULL,
     birthdate date NOT NULL,
     address varchar(255) NOT NULL,
-    classA boolean NOT NULL DEFAULT 0,
-    blacklisted boolean NOT NULL DEFAULT 0,
-    deleted boolean NOT NULL DEFAULT 0,
+    isClassA boolean NOT NULL DEFAULT 0,
+    isBlacklisted boolean NOT NULL DEFAULT 0,
+    isDeleted boolean NOT NULL DEFAULT 0,
     blacklisted_at TIMESTAMP NULL DEFAULT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     role ENUM('Broker','Owner','Customer') NOT NULL
 );
 
-CREATE TABLE hotel(
+CREATE TABLE hotel (
     hotel_number INT NOT NULL,
     hotel_branch varchar(255) NOT NULL,
     location varchar(255) NOT NULL,
-    image LONGBLOLB NOT NULL,
-    broker_user varchar(255),
-    owner_user varchar(255),
-    suspended boolean DEFAULT 0,
-    premium boolean DEFAULT 0,
+    images LONGBLOB NOT NULL,
+    broker_user varchar(255) NOT NULL,
+    owner_user varchar(255) NOT NULL,
+    isSuspended boolean DEFAULT 0 NOT NULL,
+    isPremium boolean DEFAULT 0 NOT NULL,
     FOREIGN KEY (broker_user) REFERENCES user(username),
     FOREIGN KEY (owner_user) REFERENCES user(username),
     CONSTRAINT PK_HOTEL PRIMARY KEY (hotel_number,hotel_branch)
@@ -36,9 +36,9 @@ CREATE TABLE room(
     room_number INT NOT NULL,
     hotel_branch varchar(255) NOT NULL,
     hotel_number INT NOT NULL,
-    state bit DEFAULT 0,
-    room_type varchar(255) NOT NULL,
-    price MONEY NOT NULL,
+    isBooked boolean DEFAULT 0 NOT NULL,
+    type varchar(255) NOT NULL,
+    price DECIMAL(4,2 NOT NULL,
     FOREIGN KEY (hotel_branch) REFERENCES hotel(hotel_branch),
     FOREIGN KEY (hotel_number) REFERENCES hotel(hotel_number),
     CONSTRAINT PK_ROOM PRIMARY KEY (room_number,hotel_branch,hotel_number)
@@ -59,15 +59,15 @@ CREATE TABLE reservation(
     res_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     start_date date,
     end_date date,
-    check_in boolean DEFAULT 0,
-    check_out boolean DEFAULT 0
+    checked_in boolean DEFAULT 0,
+    checked_out boolean DEFAULT 0
 );
 
 CREATE TABLE reservation_relation(
+    res_id INT NOT NULL PRIMARY KEY,
     customer_user varchar(255) NOT NULL,
     hotel_number INT NOT NULL,
     hotel_branch varchar(255) NOT NULL,
-    res_id INT NOT NULL PRIMARY KEY,
     FOREIGN KEY (customer_user) REFERENCES user(username),
     FOREIGN KEY (hotel_number) REFERENCES hotel(hotel_number),
     FOREIGN KEY (hotel_branch) REFERENCES hotel(hotel_branch)
