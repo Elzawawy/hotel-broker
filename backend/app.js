30eda1bf8fc154f77f748e9077bde0fadd4a9801
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-17 06:48:12 +0200

    Moving to Frontend integration

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,4 @@
-// Requiring Packages - Start
-const express = require("express");
-const bodyParser = require("body-parser");
-// Requiring Packages - End
-
-// configure app
-const app = express(); // define our app using express
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-); // For parsing URL encoded Data
-app.use(bodyParser.json()); // Parse to json object
-
-//Use to the EJS Templating Engine for dynamic enjection of data into views.
-//app.use('view engine','ejs');
+const app = require("../index");
 
 // Website Routes
 
