4ff1aa9fd86a80687913f71f9b0fad838f93eb05
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-17 04:11:27 +0200

    Exporter file for Table Creation queries

---
+++ backend/db/queries/TableCreationQueries/index.js
@@ -0,0 +1,4 @@
+exports.UserTable = require("./UserTable");
+exports.HotelTable = require("./HotelTable");
+exports.RoomTable = require("./RoomTable");
+exports.RatesRelationTable = require("./RatesRelationTable");
