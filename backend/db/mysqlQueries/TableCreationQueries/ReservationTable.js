f0885408efe6b6fa5ad28fea99bbf40d53c7b4af
Elzawawy <amr22.elzawawy@ymail.com>
2018-12-17 21:23:39 +0200

    Bug Fix 1 :: Forgot ","

--- backend/db/mysqlQueries/TableCreationQueries/ReservationTable.js
+++ backend/db/mysqlQueries/TableCreationQueries/ReservationTable.js
@@ -3,7 +3,7 @@ module.exports = `CREATE TABLE reservation(
     start_date date NOT NULL,
     end_date date NOT NULL,
     check_in boolean NOT NULL DEFAULT 0,
-    check_out boolean NOT NULL DEFAULT 0
+    check_out boolean NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );`;
