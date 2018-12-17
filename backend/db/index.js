6eb6b3c88f405b98a8d89035323b42f2d4bd63c5
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-17 04:11:01 +0200

    modified error callback and table creation queries

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,20 +1,15 @@
 // we import the connection and call the function to create the connection and leave it open
 const conn = require("../connect")();
+const tablesCreation = require("./queries/TableCreationQueries");
 
 exports.databaseCreate = function(params) {
   // our db name is "hotel_reservation"
   conn.query(
-    "CREATE DATABASE IF NOT EXISTS" +
+    "CREATE DATABASE IF NOT EXISTS " +
       params +
-      "CHARACTER SET utf8 COLLATE utf8_general_ci",
+      " CHARACTER SET utf8 COLLATE utf8_general_ci",
     function(err, result) {
-      if (err)
-        console.log(
-          "Database exists or an issue so replace this statement with throw err"
-        );
-      else {
-        console.log("Database created!");
-      }
+      CheckForError(err, result, params + " Database");
     }
   );
 
@@ -28,31 +23,65 @@ exports.databaseCreate = function(params) {
 };
 
 exports.createUserTable = function() {
-  // our db name is "hotel_reservation"
+  //query to create user table
+  conn.query(tablesCreation.UserTable, function(err, result) {
+    CheckForError(err, result, "User Table");
+  });
+};
+
+exports.createHotelTable = function() {
+  // query to create hoteltable
+  conn.query(tablesCreation.HotelTable, function(err, result) {
+    CheckForError(err, result, "Hotel table");
+  });
+};
+
+exports.createRoomTable = function() {
+  // query to create hoteltable
+  conn.query(tablesCreation.RoomTable, function(err, result) {
+    CheckForError(err, result, "Room Table");
+  });
+};
+
+exports.createRatesRelationTable = function() {
+  // query to create Reservation
+  conn.query(tablesCreation.RatesRelationTable, function(err, result) {
+    CheckForError(err, result, "Rate Relation Table");
+  });
+};
+
+exports.createRequestRelationTable = function() {
+  // query to create request relation
   conn.query(
-    `CREATE TABLE user (  
-    username varchar(255) NOT NULL PRIMARY KEY,
-    email varchar(255) UNIQUE NOT NULL,
-    user_password varchar(255) NOT NULL,
-    name varchar(255) NOT NULL,
-    phone varchar(255) UNIQUE NOT NULL,
-    ssn varchar(255) UNIQUE NOT NULL,
-    birthdate date NOT NULL,
-    address varchar(255) NOT NULL,
-    isClassA boolean NOT NULL DEFAULT 0,
-    isBlacklisted boolean NOT NULL DEFAULT 0,
-    isDeleted boolean NOT NULL DEFAULT 0,
-    blacklisted_at TIMESTAMP NULL DEFAULT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE   CURRENT_TIMESTAMP,
-    role ENUM('Broker', 'Owner', 'Customer') NOT NULL
-  )`,
+    `CREATE TABLE requestsRelation(
+    res_id INT NOT NULL PRIMARY KEY,
+    customer_user varchar(255) NOT NULL,
+    hotel_number INT NOT NULL,
+    hotel_branch varchar(255) NOT NULL,
+    CONSTRAINT FK_REQUESTS_RES FOREIGN KEY (res_id) REFERENCES reservation(res_id),
+    CONSTRAINT FK_REQUESTS_USER FOREIGN KEY  (customer_user) REFERENCES user(username),
+    CONSTRAINT FK_REQUESTS_HOTEL FOREIGN KEY (hotel_number,hotel_branch) REFERENCES hotel(hotel_number,hotel_branch)
+);`,
     function(err, result) {
       if (err)
         console.log(
-          "User table create or an issue so replace this statement with throw err"
+          "Request Relation table create or an issue so replace this statement with throw err"
         );
-      else console.log("User Table created!");
+      else console.log("Request Relation Table created!");
     }
   );
+};
+
+function CheckForError(err, result, name) {
+  if (err && err.sqlMessage.toString().match("already exists")) {
+    console.log(err.sqlMessage);
+  } else if (err) throw err.sqlMessage;
+  else console.log(name + " created!");
+}
+
+exports.CreateAllTables = function CreateAllTables() {
+  exports.createUserTable();
+  exports.createHotelTable();
+  exports.createRoomTable();
+  exports.createRatesRelationTable();
 };
