b2295118f2c4f969c02ba54ccbb83e73a526fc48
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-17 04:10:14 +0200

    Room Table Query

---
+++ backend/db/queries/TableCreationQueries/RoomTable.js
@@ -0,0 +1,10 @@
+module.exports = `CREATE TABLE room (
+    room_number INT NOT NULL,
+    hotel_branch varchar(255) NOT NULL,
+    hotel_number INT NOT NULL,
+    isBooked boolean DEFAULT 0 NOT NULL,
+    type varchar(255) NOT NULL,
+    price DECIMAL(4,2) NOT NULL,
+    CONSTRAINT FK_ROOM_HOTEL FOREIGN KEY (hotel_number,hotel_branch) REFERENCES hotel(hotel_number,hotel_branch),
+    CONSTRAINT PK_ROOM PRIMARY KEY (room_number,hotel_branch,hotel_number)
+);`;
