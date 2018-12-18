5c332dfd9b18e8d3ca4035f6e462a36f428502cf
Elzawawy <amr22.elzawawy@ymail.com>
2018-12-18 18:29:19 +0200

    Updated Reservation Table according to the latest relational model.

--- backend/db/mysqlQueries/TableCreationQueries/ReservationTable.js
+++ backend/db/mysqlQueries/TableCreationQueries/ReservationTable.js
@@ -1,9 +1,10 @@
-module.exports = `CREATE TABLE reservation(
-    res_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    start_date date NOT NULL,
-    end_date date NOT NULL,
-    check_in boolean NOT NULL DEFAULT 0,
-    check_out boolean NOT NULL DEFAULT 0,
+module.exports = `CREATE TABLE Reservation(
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    StartDate date NOT NULL,
+    EndDate date NOT NULL,
+    isChecked_in boolean NOT NULL DEFAULT 0,
+    isChecked_out boolean NOT NULL DEFAULT 0,
+    isDeleted boolean NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );`;
