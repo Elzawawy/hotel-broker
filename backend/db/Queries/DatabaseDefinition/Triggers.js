e9dc7f718985f0fcf9dc43da134748da3bc90e1e
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-20 18:14:13 +0200

    triggers bug fix

--- backend/db/Queries/DatabaseDefinition/Triggers.js
+++ backend/db/Queries/DatabaseDefinition/Triggers.js
@@ -1,10 +1,10 @@
-exports.RoleBeforeInsertTrigger = `CREATE TRIGGER hotel_before_insert2 BEFORE INSERT ON Hotel 
+exports.RoleBeforeInsertTrigger = `CREATE TRIGGER hotel_before_insert BEFORE INSERT ON Hotel 
                                    FOR EACH ROW 
                                    BEGIN 
                                    CALL check_role (NEW.OwnerUser,NEW.BrokerUser); 
                                    END`;
 
-exports.RoleBeforeUpdateTrigger = `CREATE TRIGGER hotel_before_update2 BEFORE UPDATE ON Hotel 
+exports.RoleBeforeUpdateTrigger = `CREATE TRIGGER hotel_before_update BEFORE UPDATE ON Hotel 
                                    FOR EACH ROW 
                                    BEGIN 
                                    CALL check_role (NEW.OwnerUser,NEW.BrokerUser); 
