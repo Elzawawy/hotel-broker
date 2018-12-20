3fbae5f0e525477030155718d048187721f1edb4
ramikhafagi96 <ramikh.developer@gmail.com>
2018-12-20 21:41:09 +0200

    Modified(login_route,register_route,register.ejs)

--- backend/routes/login_route.js
+++ backend/routes/login_route.js
@@ -8,12 +8,7 @@ function loadLoginPage(req, res) {
 }
 
 function loginUser(req, res, next) {
-  // var user = req.body;
-  //
-  // //var result  = con.query("SELECT * FROM Users WHERE username = "+user.username+" AND  password = "user.password""))
-  // //if(!result)
-  // res.status(404).send();
-  // res.render("browse_hotels")
+
 }
 
 module.exports = function(app) {
--- backend/routes/register_route.js
+++ backend/routes/register_route.js
@@ -3,18 +3,12 @@ router.get('/',loadRegisterPage);
 router.post('/',registerUser);
 
 function loadRegisterPage(req,res){
-    res.render("register");
+    res.render("pages/register");
 }
 
 function registerUser(req,res)
 {
-    user = req.body;
-    //First Check if a user has the same username or password
-    //"SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
-    //Insert into user table
-    // res.render('browseHotels.ejs');
-    //send token using a cookie
-    res.render("user_profile",{user: user});
+
 }
 
 module.exports = function(app){
