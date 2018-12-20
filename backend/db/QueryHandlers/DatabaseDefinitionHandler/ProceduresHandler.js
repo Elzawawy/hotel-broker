49041bfae76d59fd4d5a0f89d49a535673c5dff7
Elzawawy <amr22.elzawawy@ymail.com>
2018-12-20 15:59:16 +0200

    Bug Fix 3, replaced exports instead of module.exports

--- backend/db/QueryHandlers/DatabaseDefinitionHandler/ProceduresHandler.js
+++ backend/db/QueryHandlers/DatabaseDefinitionHandler/ProceduresHandler.js
@@ -1,4 +1,4 @@
-module.exports.checkRole = function() {
+exports.checkRole = function() {
     // query to create check_role procedure.
     conn.query(databaseDefinitionQueries.roleCheckProc, function(err, result) {
       CheckForError(err, result, "check_role Procedure");
--- backend/db/QueryHandlers/DatabaseDefinitionHandler/TriggersHandler.js
+++ backend/db/QueryHandlers/DatabaseDefinitionHandler/TriggersHandler.js
@@ -1,11 +1,11 @@
-module.exports.BeforeInsertHotelTrig = function() {
+exports.BeforeInsertHotelTrig = function() {
     // query to create check_role procedure.
     conn.query(databaseDefinitionQueries.beforeInsertHotelTrig, function(err, result) {
       CheckForError(err, result, "Before Insert on Hotel Trigger ");
     });
 };
 
-module.exports.BeforeUpdateHotelTrig = function() {
+exports.BeforeUpdateHotelTrig = function() {
     // query to create check_role procedure.
     conn.query(databaseDefinitionQueries.beforeUpdateHotelTrig, function(err, result) {
       CheckForError(err, result, "Before Update on Hotel Trigger ");
