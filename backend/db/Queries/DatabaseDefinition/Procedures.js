beb7d3c5e18e48f3b834a22b3373bb44f2bbac0d
Elzawawy <amr22.elzawawy@ymail.com>
2018-12-20 12:03:07 +0200

    Added check_role Procedure to add check constraint on teh values of usernames in Hotel.

---
+++ backend/db/Queries/DatabaseDefinition/Procedures.js
@@ -0,0 +1,22 @@
+exports.checkRole = `DELIMITER $$
+                    CREATE PROCEDURE check_role
+                    ( IN owner_user varchar(255), IN broker_user varchar(255))
+                    BEGIN
+                    DECLARE countdata,countdata2 INT;
+                    SELECT COUNT(1) INTO countdata FROM User
+                    WHERE Username = broker_user and Role = 'Broker'; 
+                    SELECT COUNT(1) INTO countdata FROM User
+                    WHERE Username = owner_user and Role = 'Owner'; 
+                    IF ( countdata > 0)
+                    THEN 
+                    SIGNAL SQLSTATE '45001'
+                    SET MESSAGE_TEXT = 'Check Constraint on BrokerUser failed';
+                    END IF;
+                    IF ( countdata2 > 0)
+                    THEN 
+                    SIGNAL SQLSTATE '45001'
+                    SET MESSAGE_TEXT = 'Check Constraint on OwnerUser failed';
+                    END IF;
+                    END$$
+                    DELIMITER ;
+                    `;
