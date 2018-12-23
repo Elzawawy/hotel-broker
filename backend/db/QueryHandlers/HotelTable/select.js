e6a011a576da466135b8c1ae835300d3066885d2
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-23 21:14:45 +0200

    QueryHandler added

--- backend/db/QueryHandlers/HotelTable/select.js
+++ backend/db/QueryHandlers/HotelTable/select.js
@@ -1,11 +1,11 @@
-module.exports = function(params) {
+module.exports = function(req, res, params, callback) {
   // query to insert into hotel table.
-  return conn.query(hotelQueries.hotelSelect, params, function(
+  conn.query(hotelQueries.hotelSelect.getAllHotels, params, function(
     err,
     result,
     fields
   ) {
     CheckForError(err, result, "Hotel table select");
-    console.log(result);
+    callback(result);
   });
 };
