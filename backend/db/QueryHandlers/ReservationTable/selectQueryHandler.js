d79d83daec5344c3fa95c0af1ce983a3a566b737
Elzawawy <amr22.elzawawy@ymail.com>
2018-12-23 19:16:36 +0200

    Callback fucntion added as a workaround for Asynchronous calls.

--- backend/db/QueryHandlers/ReservationTable/selectQueryHandler.js
+++ backend/db/QueryHandlers/ReservationTable/selectQueryHandler.js
@@ -1,9 +1,9 @@
 //retrieveUserResList() method should be called to get a reservations list (curret and previous) 
 //params should be in the form ['username'] and returns a list of results !
-exports.retrieveUserResList = function (params) {
+exports.retrieveUserResList = function (params,callback) {
     //query to retrieve Username if username and password exists, otherwise returns NULL
     conn.query(resQueries.resSelectQueries.retrieveUserResList, params, function (err, result, fields) {
         if(err) console.log(err);
-        else return result;
+        else callback(result);
     });
 };
