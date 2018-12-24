775f0405a4a31c89cb1c07cb7fd5dd1753c6a88d
ramikhafagi96 <ramikh.developer@gmail.com>
2018-12-24 18:38:00 +0200

    Broker Route Under Development....(Pushed for frontend to continue work)

--- backend/db/QueryHandlers/HotelTable/select.js
+++ backend/db/QueryHandlers/HotelTable/select.js
@@ -1,11 +1,21 @@
-module.exports = function(req, res, params, callback) {
+exports.showAllHotels = function(req, res, callback) {
   // query to insert into hotel table.
-  conn.query(hotelQueries.hotelSelect.getAllHotels, params, function(
+  conn.query(hotelQueries.hotelSelect.getAllHotels, function(
     err,
     result,
-    fields
   ) {
     CheckForError(err, result, "Hotel table select");
-    callback(result);
+    callback(req,res,JSON.stringify(result));
   });
+};
+
+exports.showClassAUsers = function(req, res, callback) {
+    // query to insert into hotel table.
+    conn.query(hotelQueries.hotelSelect.getAllHotels, function(
+        err,
+        result,
+    ) {
+        CheckForError(err, result, "Hotel table select");
+        callback(req,res,JSON.stringify(result));
+    });
 };
---
+++ backend/routes/brocker_route.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+
+router.get("/",loadBrockerPage);
+router.get("/showHotels",loadAllHotels);
+// router.get("/classAUsers",loadClassAUsers);
+
+function loadBrockerPage(req,res){
+    res.render("pages/broker");
+}
+
+function loadAllHotelsCB(req,res,result){
+    let hotels = JSON.parse(result);
+    console.log(hotels);
+    res.render("pages/broker_show_hotels",{hotels: hotels});
+}
+
+function loadAllHotels(req,res){
+    query.hotelQueriesHandler.hotelSelect.showAllHotels(req,res,loadAllHotelsCB);
+}
+
+// function loadClassAUsersCB(req,res,result){
+//
+// }
+//
+// function loadClassAUsers(req,res){
+//     query.
+// }
+
+
+module.exports = function(app) {
+    app.use("/broker", router);
+};
