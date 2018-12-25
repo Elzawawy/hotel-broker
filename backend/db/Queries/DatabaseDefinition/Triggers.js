f9ec489bd5e5dd0bd83dd7ff2538f021ad3d10f4
Elzawawy <amr22.elzawawy@ymail.com>
2018-12-25 23:04:55 +0200

    Trigger and Procedure check type on reservation are done.

--- backend/db/Queries/DatabaseDefinition/Triggers.js
+++ backend/db/Queries/DatabaseDefinition/Triggers.js
@@ -9,3 +9,15 @@ exports.RoleBeforeUpdateTrigger = `CREATE TRIGGER hotel_before_update BEFORE UPD
                                    BEGIN 
                                    CALL check_role (NEW.OwnerUser,NEW.BrokerUser); 
                                    END;`;
+
+exports.TypeBeforeInsertTrigger = `CREATE TRIGGER reservation_before_insert BEFORE INSERT ON Reservation
+                                  FOR EACH ROW 
+                                  BEGIN
+                                  CALL check_reservation_type(NEW.RoomNumber, NEW.HotelName, NEW.HotelBranch);
+                                  END;`;
+
+exports.TypeBeforeInsertTrigger = `CREATE TRIGGER reservation_before_update BEFORE UPDATE ON Reservation
+                                  FOR EACH ROW 
+                                  BEGIN
+                                  CALL check_reservation_type(NEW.RoomNumber, NEW.HotelName, NEW.HotelBranch);
+                                  END;`;
