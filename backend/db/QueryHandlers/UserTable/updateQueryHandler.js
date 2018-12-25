6f343143b5bafd293ebffc0e8af7e4ea9f537c4e
ramikhafagi96 <ramikh.developer@gmail.com>
2018-12-25 21:17:16 +0200

    Fixed Callback Functions

--- backend/db/QueryHandlers/UserTable/updateQueryHandler.js
+++ backend/db/QueryHandlers/UserTable/updateQueryHandler.js
@@ -1,14 +1,10 @@
 //This Function is used to update a User information in system. 
 //Params is a JSON object { field : fieldval , .....}
-exports.updateUserInfo = function(res,params) {
+exports.updateUserInfo = function(req,res,params,callback) {
     // query to update into User table.
     conn.query(userQueries.userUpdateQueries.updateUserInfo, params, function(err, result) {
       if(err) return err;
-      if(params[0].isBlacklisted === 1){
-          res.redirect("/broker/showAllUsers");
-      }
-      if(params[0].isBlacklisted === 0){
-          res.redirect("/broker/showSuspendedUsers");
-      }
+
+      else callback(req,res);
     });
 };
