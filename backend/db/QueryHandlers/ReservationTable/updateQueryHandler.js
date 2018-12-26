4f3d7da857413852bbcde54dfe105eaa43506940
ramikhafagi96 <ramikh.developer@gmail.com>
2018-12-26 10:33:36 +0200

    Reservation Requests Accept or Deny

--- backend/db/QueryHandlers/ReservationTable/updateQueryHandler.js
+++ backend/db/QueryHandlers/ReservationTable/updateQueryHandler.js
@@ -1,10 +1,17 @@
 //This function approves a reservation can be made on this hotel. 
 //params should be in the form of [ID];
-exports.approveReservation = function(params,callback){
+exports.ApproveReservation = function(req,res,params,callback){
     conn.query(resQueries.resUpdateQueries.approveReservation, params, function(err, result, fields) {
         if(err) console.log(err);
-        else callback(result);
+        else callback(req,res);
       });
+};
+
+exports.DenyReservation = function(req,res,params,callback){
+    conn.query(resQueries.resUpdateQueries.denyReservation, params, function(err, result, fields) {
+        if(err) console.log(err);
+        else callback(req,res);
+    });
 };
 
 //This function checks in a reservation when a user shows up. 
