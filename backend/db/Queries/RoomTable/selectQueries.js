9ae745cf96531c360c89b35194ae531586f54acb
Amr Ramadan <amr_ramadan97@outlook.com>
2018-12-26 10:00:15 +0200

    get free rooms query updated

--- backend/db/Queries/RoomTable/selectQueries.js
+++ backend/db/Queries/RoomTable/selectQueries.js
@@ -3,16 +3,5 @@ exports.getAllHotelRooms = `SELECT Number,Type,Price,IF(checkedOut > CURRENT_TIM
                             FROM Room WHERE HotelName = ? AND HotelBranch = ?  AND Number > 0 AND isDeleted = 0`;
 
 //query that returns free rooms only in a specific hotel.
-//fi moshkla hena en law room mat7gztsh abl keda msh htzhr. 
-exports.getFreeRooms = `SELECT Number,price FROM Room LEFT JOIN Reservation 
-                        ON Number = RoomNumber 
-                        AND Room.HotelName = Reservation.HotelName 
-                        AND Room.HotelBranch = Reservation.HotelBranch 
-                        AND Room.HotelName = ? 
-                        AND Room.HotelBranch = ?
-                        AND Room.checkedOut <= ? 
-                        AND Reservation.StartDate >= ?
-                        AND Room.isDeleted = 0
-                        AND Reservation.isDeleted = 0
-                        AND Room.Number > 0
-                        LIMIT 1`;
+//fi moshkla hena en law room mat7gztsh abl keda msh htzhr.
+exports.getFreeRooms = `SELECT room.Number from room where room.Number NOT IN( SELECT room.Number FROM Room JOIN Reservation WHERE Number = RoomNumber AND Room.HotelName = Reservation.HotelName AND Room.HotelBranch = Reservation.HotelBranch AND Room.HotelName = "ea" AND Room.HotelBranch = "Jamaalview" AND Room.checkedOut > "2018-12-26" AND Reservation.StartDate < "2018-12-30" AND Room.isDeleted = 0 AND Reservation.isDeleted = 0 AND Room.Number > 0 ) AND Room.HotelName = "ea" AND Room.HotelBranch = "Jamaalview" AND Room.isDeleted = 0 AND Room.Number > 0 LIMIT 1`;
